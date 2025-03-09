7f51d555b2e1a7b1380cc788c5d16f84be28ebdb
Herber Batres <hbatress@miumg.edu.gt>
2025-03-08 21:33:31 -0600

    apis funcionando

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     title="Inventarios de activos",
+ *     version="1.0.0",
+ *     description="APIs para el manejo de inventarios de activos"
+ * )
+ */
 abstract class Controller
 {
-    //
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+ /**
+     * @OA\Get(
+     *     path="/api/ejemplo",
+     *     summary="Obtener ejemplo",
+     *     description="Obtiene un ejemplo",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Respuesta exitosa"
+     *     )
+     * )
+     */
+    public function obtenerEjemplo()
+    {
+        return response()->json([
+            'mensaje' => 'Este es un ejemplo de respuesta',
+            'data' => [
+                'id' => 1,
+                'nombre' => 'Ejemplo'
+            ]
+        ], 200);
+    }
 }
